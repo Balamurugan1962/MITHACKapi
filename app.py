a5058cb1a04bd34b14eb7a71a158ce4002f45b2e
Balamurugan1962 <145089896+Balamurugan1962@users.noreply.github.com>
2025-04-05 10:04:07 +0530

    added post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,jsonify
-from flask_restful import Api,Resource
+from flask_restful import Api,Resource,reqparse
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,9 +7,19 @@ cors = CORS(app,origins=['*'])
 api = Api(app)
 
 
+Init_put = reqparse.RequestParser()
+Init_put.add_argument("Test",location = 'form',help='Give Text for the test')
+
+
 class Init(Resource):
     def get(self):
         return jsonify({"Reply": "Hosted successfully"})
+    
+    def post(self):
+        data = Init_put.parse_args()
+        reply = {}
+        reply["Test"] = data["Test"]
+        return jsonify(reply)
     
 
 api.add_resource(Init,'/')
